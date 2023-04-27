095623e24989246805862bece20a33d1e402b2e0
Arkoprabho Bhattacharjee <bhattacharjee.arkoprabho@gmail.com>
2023-04-27 20:12:48 +0530

    First commit minus all the boilerplate-y stuff

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Arkoprabho Bhattacharjee | Digital Product Designer & Developer</title>
+        <meta name="description" content="Arkoprabho Bhattacharjee's Portfolio Website'" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='w-screen min-h-screen'></main>
+    </>
+  )
+}
