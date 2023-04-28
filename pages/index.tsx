f6881167cb03f9ae088f712e75a532b95e0f9f3c
Arkoprabho Bhattacharjee <bhattacharjee.arkoprabho@gmail.com>
2023-04-28 12:38:44 +0530

    Minor description meta update

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Arkoprabho Bhattacharjee | Digital Product Designer & Developer</title>
-        <meta name="description" content="Arkoprabho Bhattacharjee's Portfolio Website'" />
+        <meta name="description" content="Arkoprabho Bhattacharjee's Portfolio Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
